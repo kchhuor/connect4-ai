4b0d258c217d6613aeb733db163c8358c0021945
aijahlin <aijahlin@csu.fullerton.edu>
2026-04-27 15:09:40 -0700

    added functions for playing game pieces

--- backend/game.py
+++ backend/game.py
@@ -21,3 +21,17 @@ AI_PIECE = 2
 def create_board():
     board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
+
+# Drops a game piece onto board at a specific position
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+# Checks if a column is a valid place to drop a piece. Return a boolean
+def is_valid_location(board, col):
+    return board[ROW_COUNT-1][col] == 0
+
+# Finds and returns next open row in a given column
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
